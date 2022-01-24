e033feeb75f0f4ffe764d82f491ebc676f1cf6b7
Murarireddy <91661858+murarireddy108@users.noreply.github.com>
2022-01-24 08:57:09 -0800

    day-19

---
+++ New folder (2)/script.js
@@ -0,0 +1,12 @@
+let list = document.querySelector(".pagination")
+list.innerHTML = `<li class="page-item">
+<span class="page-link">Previous</span></li>
+<li class="page-item"><a class="page-link" href="#" tabindex="-1">1</a></li>
+<li class="page-item"><a class="page-link" href="#">2</a></li>
+<li class="page-item"><a class="page-link" href="#">3</a></li>    
+<li class="page-item"><a class="page-link" href="#">4</a></li>    
+<li class="page-item"><a class="page-link" href="#">5</a></li>    
+<li class="page-item active"><a class="page-link" href="#">6</a></li>    
+<li class="page-item"><a class="page-link" href="#">7</a></li>    
+<li class="page-item"><a class="page-link" href="#">8</a></li>    
+<li class="page-item"><a class="page-link" href="#">Next</a></li>`;
